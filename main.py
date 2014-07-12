30ba00554c64ca6bd6cdf9dd501c36999fa1432e
Leon Sasson <leonsassonha@gmail.com>
2014-07-11 19:30:28 -0500

    switched to sparkfun backend

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
 import glob
 import time, datetime
-import tempodb
+from phant import Phant
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     temp_c, temp_f = None, None
     lines = read_temp_raw()
@@ -26,36 +26,24 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_c, temp_f
+    return temp_c
 
 def init_client():
-    fname = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir)) + "/tempodb_keys"
-    with open(fname,'r') as f:
-        api_key = f.readline().strip()
-        api_secret = f.readline().strip()
+    fname = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir)) + "/data_sparkfun_keys"
 
-    print api_key, api_secret
-    return tempodb.Client(api_key, api_secret)
+    with open(fname,'r') as f:
+        public_key = f.readline().strip()
+        private_key = f.readline().strip()
+
+    return Phant(public_key, 'c',private_key=private_key)
 
 def main():
-    temp_c, temp_f = read_temp()
-    now = datetime.datetime.now()
-    data = []
+    temp_c = read_temp()
 
     if temp_c:
-        data.append({'key': 'sensor-type:temperature.temp-unit:C.1',
-                    'v': temp_c
-                    })
-
-    if temp_f:
-        data.append({'key': 'sensor-type:temperature.temp-unit:F.1',
-                    'v': temp_f
-                    })
-
-    client = init_client()
-    print now, data
-    client.write_bulk(now, data)
+        client = init_client()
+        print temp_c
+        client.log(temp_c)
 
 if __name__ == '__main__':
     main()
